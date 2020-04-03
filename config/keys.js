1cae34dd0b1d71f2902fc73baa109bbb0aebe89f
giorgi17 <giorgi199817@gmail.com>
2020-04-03 07:13:11 +0400

    configured and connected to mongodb

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://stock-twits-root:twits123@stocktwitsdb-ukbci.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://voice-app-user:voice-app-17@cluster0-y0nsa.mongodb.net/test?retryWrites=true&w=majority",
     secretOrKey: "secret" 
   };
